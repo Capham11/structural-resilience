3d523989c962e70794461762826c5897dc22dce8
capham11 <christopherpham@outlook.com>
2026-09-22 13:58:50 -0700

    update name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -758,8 +758,8 @@ export default function App() {
     <div className="app">
       <header className="header">
         <div className="header-left">
-          <span className="header-tag">Sevengill Labs</span>
-          <h1 className="header-title">Mako Epidemic Intelligence (WA Beta)</h1>
+          <span className="header-tag">Sevengills</span>
+          <h1 className="header-title">Epidemic Intelligence (WA Beta)</h1>
         </div>
         <div className="header-center"><span className="header-status">{statusMsg}</span></div>
         <div className="header-right">
